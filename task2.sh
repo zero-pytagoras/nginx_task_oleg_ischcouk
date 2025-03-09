65fc6d5d08ef14a4902333eac7eb4f5524097623
zmey <greenzmey@gmail.com>
2025-03-09 13:51:17 +0200

    t new repo

---
+++ task2.sh
@@ -0,0 +1,10 @@
+#########################################
+#Develop by: Oleg Ischouk
+#Purpose:
+#Date: 28/02/2025
+#Version: 1.0.0
+set -o errexit
+#set -o pipefail
+########################################
+
+
